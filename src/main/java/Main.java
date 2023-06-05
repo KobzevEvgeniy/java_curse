75638656e2e69fd5f7c6b32095d9475a8f7c0f80
Evgeniy <public-judge@yandex.ru>
2023-06-05 22:17:23 +0300

    Save first program from seminar 1

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        int i= 'g';
+        long l;
+        short s;
+        char ch= 124;
+        float f= .3455f;
+        double d=.34657;
+        boolean  aBoolean= false ;
+        int[] ints = new int[12];
+        String s1= "Hello World";
+        s1 = s1+"!";
+
+
+        System.out.println(s1);
+
+
+    }
+
+}
---
+++ src/main/java/S1.java
@@ -0,0 +1,8 @@
+public class S1 {
+    public static void main(String[] args) {
+        String s1="Hello world";
+
+        System.out.println(s1);
+
+    }
+}
